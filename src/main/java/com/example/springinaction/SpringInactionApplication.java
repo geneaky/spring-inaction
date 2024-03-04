7767012441b12605e793e1fa8d259c462353f405
geneaky <kaktus418@gmail.com>
2024-03-04 21:40:29 +0900

    first commit

---
+++ src/main/java/com/example/springinaction/SpringInactionApplication.java
@@ -0,0 +1,13 @@
+package com.example.springinaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringInactionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringInactionApplication.class, args);
+    }
+
+}
